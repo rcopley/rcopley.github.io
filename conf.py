0ae172dc613bf21381eeab37d0ff3026798e60ba
Ronald Copley <ronald@ronaldcopley.com>
2024-01-20 22:33:18 -0500

    Started documentation of Battelle shmoocon ctf.

---
+++ conf.py
@@ -0,0 +1,27 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'CTF Writeups and Ramblings'
+copyright = '2024, Ronald Copley'
+author = 'Ronald Copley'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["sphinxawesome_theme.highlighting"]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.venv']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinxawesome_theme'
+html_static_path = ['_static']
---
+++ index.rst
@@ -0,0 +1,15 @@
+.. CTF Writeups documentation master file, created by
+   sphinx-quickstart on Mon Jan 15 13:56:13 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+CTF Writeups and Ramblings
+==========================
+
+.. toctree::
+   :maxdepth: 1
+   :caption: 2024 Battelle Shmoocon CTF
+   :glob:
+
+   ctfs/2024/battelle-shmoocon/*
+
